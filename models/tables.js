1dae52da146c5e08c52bafbf87e21bd2fffaed79
giginlila <gigeej009@gmail.com>
2023-12-19 13:13:22 +1030

    applied basic file structure & dependencies

---
+++ models/tables.js
@@ -0,0 +1,10 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class table extends Model {}
+
+// apply table.init() function
+//
+//
+
+module.exports = table;
