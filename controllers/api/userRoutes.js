0055045f08a17dbc296ad16429a1b1080b82635c
BowieRC <rowlandscrispebowie@gmail.com>
2024-01-02 00:43:05 +0930

    Update login and out routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,32 +18,36 @@ router.post('/', async (req, res) => {
 
 router.post("/login", async (req, res) => {
     try {
-        const userData = await User.findOne({where: {username: req.body.email} });
-
+        console.log(req.body[0]);
+        console.log(req.body[0].email);
+        const userData = await User.findOne({where: {email: req.body[0].email} });
+        
         if(!userData) {
             res.status(400)
             .json({message: 'Incorrect combination, try again'});
             return;
         }
+        const validPassword = await userData.checkPassword(req.body[0].password);
 
-        // !!! Add check password to user model !!!
-
-        // const validPassword = await userData.checkPassword(req.body.password);
-
-        // if(!validPassword) {
-        //     res.status(400)
-        //     .json({message: "Incorrect combination, try again"});
-        // }
+        if(!validPassword) {
+            res.status(400)
+            .json({message: "Incorrect combination, try again"});
+        }
 
         req.session.save(() => {
-            req.session.user_id = userData.id;
+            req.session.user_id = userData.user_id;
             req.session.logged_in = true;
 
-            res.json({ user: userData, message: "Logged In"});
+            res.json({message: "Logged In"});
+            console.log(req)
         })
 
     } catch(err) {
         res.status(400).json(err);
+        console.log("Username", req.body.username);
+        console.log("Password", req.body.password);
+        console.log(req.body);
+        console.log(err);
     }
 });
 
@@ -71,10 +75,26 @@ router.get('/all', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        const userData = await User.findByPk(req.params.id);
-        res.status(200).json(userData);
+        if(req.session.logged_in){
+            if(req.session.user_id == req.params.id){
+                const userData = await User.findOne({where: {
+                    user_id: req.params.id,
+                }});
+                res.status(200).json(userData);
+            } else {
+                res.status(200).json({message: "Cannot access someone elses account!"});
+                return;
+            }
+            console.log(req.session)
+        
+        } else {
+            res.status(200).json({message: "Please Log In"});
+
+            console.log("please log in");
+            return;
+        }
     } catch(err) {
-        res.status(400).json(err);
+        res.status(500).json(err);
         console.log(err);
     }
 })
