822707db57d3cd210e6d7fe33bc2febe739d3375
BowieRC <rowlandscrispebowie@gmail.com>
2024-01-01 17:40:22 +0930

    created seeds for testing

---
+++ seeds/index.js
@@ -0,0 +1,16 @@
+const sequelize = require('../config/connection');
+const User = require('../models/user');
+const userData = require('./userData.json');
+
+const seedUserData = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedUserData();
